18c5a3890d14a114d09df124d2e139ecb8641bad
Angela <angela.sloma@gmail.com>
2018-01-26 00:55:46 +0100

    :

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+/* button formularza kontaktowego */
+<script>
+    function formHide()
+    var x = document.getElementById("formShow");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block"}
+     else {
+        x.style.display = "none"}
+}
+</script>
