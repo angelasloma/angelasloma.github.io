883f07c36bdd657f43eab3849cb4c5138464c75b
Angela <angela.sloma@gmail.com>
2018-01-27 03:36:24 +0100

    :margin

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,9 @@
-/* button formularza kontaktowego */
-<script>
-    function formHide() {
-        var a = document.getElementById('formShow');
-</script>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
